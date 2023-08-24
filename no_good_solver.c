71549953c31a01f6edf2e399190046b0bbe4ab3d
santi <santi@DESKTOP-HH3OVR2>
2023-08-24 14:28:24 +0200

    unitProp

--- no_good_solver.c
+++ no_good_solver.c
@@ -34,14 +34,16 @@ void solve();
 void unitPropagation();
 void backJump();
 void pureLiteralCheck();
-
+void removeNoGoodSetsContaining(int,int);
 
 int noVars=0; //the number of vars
-int noNoGoods=0; //the no of clauses
-int falsifiedNoGoods=0; //the number of non satisfied clauses
+int noNoGoods=0; //the no of clauses (initial)
+int currentNoGoods=0; //the number of non satisfied clauses (yet)
 int **matrix=NULL; //the matrix that holds the clauses
 int *partialAssignment;//we skip the cell 0 in order to maintain coherence with the var numbering
-int* varBothNegatedAndNot = NULL;
+int *varBothNegatedAndNot = NULL; //a int array that holds the status of the variables in the clauses (see the defines above)
+int *noOfVarPerNoGood = NULL; //a int array that holds the number of variables in each clause
+int *lonelyVar = NULL; //a int array that holds if noOfVarPerNoGood[i]==1 the index of the only variable in the clause
 
 void main(int argc, char const *argv[]){
 	
@@ -55,12 +57,14 @@ void main(int argc, char const *argv[]){
     printMatrix();
     printf("The status of the variables in the clauses: (%d doesn't appear, %d just positive, %d just negative, %d both)\n",FIRST_APPEARENCE, APPEARS_ONLY_POS,   APPEARS_ONLY_NEG,   APPEARS_BOTH);
     printVarArray(varBothNegatedAndNot);
-
+    printf("\n");
 	partialAssignment=(int *) calloc(noVars+1, sizeof(int));
+
+
 	
     
 	pureLiteralCheck();
-
+    printMatrix();
     solve();
 
     deallocateMatrix();
@@ -113,7 +117,7 @@ void readFile_allocateMatrix(const char *str){
 	printf("number of vars: %d \n",noVars);
     printf("number of nogoods: %d \n",noNoGoods);
 
-    falsifiedNoGoods=noNoGoods;
+    currentNoGoods=noNoGoods;
 
     popualteMatrix(ptr, varBothNegatedAndNot);
     
@@ -125,28 +129,36 @@ void popualteMatrix(FILE* ptr){
    
 	allocateMatrix();
     varBothNegatedAndNot = (int *)calloc(noVars + 1, sizeof(int));
+    noOfVarPerNoGood = (int *)calloc(noNoGoods, sizeof(int));
+    lonelyVar = (int *)calloc(noNoGoods, sizeof(int));
+
     for(int i = 0; i < noVars + 1; i++) {
 		varBothNegatedAndNot[i] = FIRST_APPEARENCE;
 	}
+
     int clauseCounter=0;
 	int literal=0;
 
     while(!feof(ptr) && clauseCounter<noNoGoods){
 		fscanf (ptr, "%d", &literal);
 		if(literal==0){
-			matrix[clauseCounter][0]=UNSATISFIED; 
+			matrix[clauseCounter][0]=UNSATISFIED; //the first cell of the matrix is the status of the clause
 			clauseCounter++;
 		}else{
+           
             int sign = literal > 0 ? POSITIVE_LIT : NEGATED_LIT;
             matrix[clauseCounter][literal*sign] = sign;
+            noOfVarPerNoGood[clauseCounter]++;
+            //if i have more vars i won't read this, so it can contain a wrong value
+            lonelyVar[clauseCounter] = literal * sign;
 
+            //populate the varBothNegatedAndNot array
             if(varBothNegatedAndNot[literal * sign]==FIRST_APPEARENCE)
 				varBothNegatedAndNot[literal * sign]=sign;
             if(varBothNegatedAndNot[literal * sign]==APPEARS_ONLY_POS && sign==NEGATED_LIT)
                 varBothNegatedAndNot[literal * sign]=APPEARS_BOTH;
 			if(varBothNegatedAndNot[literal * sign]==APPEARS_ONLY_NEG && sign==POSITIVE_LIT)
 				varBothNegatedAndNot[literal * sign]=APPEARS_BOTH;
-
 		}
 	}
 
@@ -193,27 +205,47 @@ void printVarArray(int *array) {
 }
 
 void solve(){
-    if (falsifiedNoGoods==0) {
-        printf("SATISFIABLR\n");
+    if (currentNoGoods==0) {
+        printf("SATISFIABLE\n");
+        printf("Assignment:\n");
+        printVarArray(partialAssignment);
 		return;
     }
 }
 
 void unitPropagation(){
-    
+    for (int i = 0; i < noNoGoods; i++) {
+		if (noOfVarPerNoGood[i] == 1) {
+            //lonelyVar[i] is a column index
+            partialAssignment[lonelyVar[i]]= matrix[i][lonelyVar[i]] > 0 ? FALSE : TRUE;
+            removeNoGoodSetsContaining(lonelyVar[i], partialAssignment[lonelyVar[i]] == TRUE ? NEGATED_LIT : POSITIVE_LIT);
+        }
+	}
 }
 void pureLiteralCheck(){
     for (int i = 1; i < noVars; i++) {
         if (varBothNegatedAndNot[i] == APPEARS_ONLY_POS) {
 			partialAssignment[i] = FALSE;
-            falsifiedNoGoods=-noclauseswuth;
+            removeNoGoodSetsContaining(i,POSITIVE_LIT);
 		} else if (varBothNegatedAndNot[i] == APPEARS_ONLY_NEG) {
 			partialAssignment[i] = TRUE;
-            falsifiedNoGoods = -noclauseswuth;
+            removeNoGoodSetsContaining(i,NEGATED_LIT);
         }
     }
 }
 
 void backJump(){
 
+}
+
+void removeNoGoodSetsContaining(int varIndex,int sign) {
+
+    //scan column (varIndex) of matrix
+    for (int i = 0; i < noNoGoods; i++) {
+		if (matrix[i][varIndex] == sign) {
+			//remove the nogood set
+            matrix[i][0] = SATISFIED;
+			currentNoGoods -= 1;
+		}
+	}
 }
