ee0d935d0c7cb6bce9fa5673193fbf96ab05cbd6
Enrico <59199308+EnrSanti@users.noreply.github.com>
2023-09-08 14:31:28 +0200

    prepared no_good_solver.c for common.h

--- no_good_solver.c
+++ no_good_solver.c
@@ -2,43 +2,7 @@
 #include <string.h>
 #include <stdbool.h> 
 #include <stdlib.h>
-
-//for the clauses
-#define UNSATISFIED -2
-#define SATISFIED 2
-
-//for unit propagation
-#define CONFLICT -3
-#define NO_CONFLICT 3
-
-//for the problem
-#define SATISFIABLE true
-#define UNSATISFIABLE false
-
-//for the literals in the matrix
-#define NEGATED_LIT -1
-#define POSITIVE_LIT 1
-#define NO_LIT 0
-//for the literals in the partial assignment
-#define UNASSIGNED 0
-#define TRUE 1
-#define FALSE -1
-
-//for the pure literal check
-#define FIRST_APPEARENCE 0
-#define APPEARS_ONLY_POS 1
-#define APPEARS_ONLY_NEG -1
-#define APPEARS_BOTH 3
-
-struct NoGoodData {
-    int currentNoGoods; //the number of non satisfied clauses (yet)
-    int** matrix; //the matrix that holds the clauses
-    int* partialAssignment;//we skip the cell 0 in order to maintain coherence with the var numbering
-    int* noOfVarPerNoGood; //a int array that holds the number of variables in each clause
-    int* lonelyVar; //a int array that holds if noOfVarPerNoGood[i]==1 the index of the only variable in the clause
-    int varsYetToBeAssigned; //the number of variables that are not yet assigned
-    int *varsAppearingInRemainingNoGoods; //a int array keeping track for each variable in how many no goods it shows up
-};
+#include "common.h"
 
 void readFile_allocateMatrix(const char *, struct NoGoodData*);
 void printError(char *);
@@ -328,7 +292,7 @@ bool solve(struct NoGoodData data, int var, int value) {
     return true;
  }
 
-//performs the unit propagation 
+//performs the unit propagation (quadratic wrt no goods in worst case, can be improved obviously)
 int unitPropagation(struct NoGoodData* data){
     //for each no good
     for (int i = 0; i < noNoGoods; i++) {
